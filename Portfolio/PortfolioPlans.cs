60792cb521b81035b49b5d5f238cf055640087e0
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-07-17 22:03:11 -0600

    Created a Portfolio Plan

---
+++ Portfolio/PortfolioPlans.cs
@@ -0,0 +1,25 @@
+/*
+/// This document is designed to layout a gameplan for how I am going to continue to commit until the portfolio deadline
+    If not in this document another will be made to compare how I am doing with said plan.
+
+    Week 1 (7/17 to 7/23)
+    
+    GOALS: 
+    1) Create an Artstation Account and make sure all other platforms are ready to have uploads on them
+    2) Update UVs for current Workshop models
+    3) Decide what new models to add to the workshop
+    4) Model 3 of the new models
+    5) Finish 1 of the 3 models ready to render
+
+    Updates:
+    1)
+
+    Results:
+    1)
+
+
+
+
+
+
+*/
